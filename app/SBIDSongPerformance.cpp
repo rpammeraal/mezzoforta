76ea9973eb63b4df6e4eaac01d4f1749bf741c89
Roy P. Ammeraal <rpammeraal@gmail.com>
2016-11-27 19:34:17 -0800

    20161127: -	Split up SBIDPerformance into SongSBIDPerformance and SBIDRecordPerformance, which inherits the former -	Can we get rid of itemID()? No!

---
+++ app/SBIDSongPerformance.cpp
@@ -0,0 +1,6 @@
+#include "SBIDPerformance.h"
+
+SBIDPerformance::SBIDPerformance()
+{
+
+}
---
+++ app/SBIDSongPerformance.h
@@ -0,0 +1,11 @@
+#ifndef SBIDPERFORMANCE_H
+#define SBIDPERFORMANCE_H
+
+
+class SBIDPerformance
+{
+public:
+    SBIDPerformance();
+};
+
+#endif // SBIDPERFORMANCE_H
